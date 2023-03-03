3eb6e5b50345ec0d5847aadf84192521df72a2c3
Victor Shyba <victor.shyba@gmail.com>
2023-03-03 20:06:16 -0300

    test round robin

--- src/resolver.rs
+++ src/resolver.rs
@@ -1,9 +1,8 @@
 use std::net::SocketAddr;
-use std::io;
 
 pub struct DNSResolver {
     upstream: Vec<SocketAddr>,
-    strategy: ResolutionStrategy
+    strategy: ResolutionStrategy,
 }
 
 pub enum ResolutionStrategy {
@@ -13,7 +12,10 @@ pub enum ResolutionStrategy {
 
 impl Default for DNSResolver {
     fn default() -> Self {
-        Self { upstream: Default::default(), strategy: ResolutionStrategy::RoundRobin }
+        Self {
+            upstream: Default::default(),
+            strategy: ResolutionStrategy::RoundRobin,
+        }
     }
 }
 
@@ -21,7 +23,25 @@ impl DNSResolver {
     pub fn pick_upstream(&self, request_id: u16) -> &[SocketAddr] {
         match self.strategy {
             ResolutionStrategy::All => &self.upstream[..],
-            ResolutionStrategy::RoundRobin => std::slice::from_ref(&self.upstream[request_id as usize % self.upstream.len()])
+            ResolutionStrategy::RoundRobin => {
+                std::slice::from_ref(&self.upstream[request_id as usize % self.upstream.len()])
+            }
         }
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+    #[test]
+    fn test_round_robin() {
+        let upstream = vec!["1.1.1.1:53".parse().unwrap(), "1.0.0.1:53".parse().unwrap()];
+        let resolver = DNSResolver {
+            upstream: upstream.clone(),
+            strategy: ResolutionStrategy::RoundRobin,
+        };
+        assert_eq!(&[upstream[0]], resolver.pick_upstream(0));
+        assert_eq!(&[upstream[1]], resolver.pick_upstream(1));
+        assert_eq!(&[upstream[0]], resolver.pick_upstream(2));
     }
 }
