0c89e04c16231aca1c58dfe97c5ae8ebd2c46b6f
Victor Shyba <victor.shyba@gmail.com>
2023-03-03 22:02:58 -0300

    send concurrently

--- src/resolver.rs
+++ src/resolver.rs
@@ -1,8 +1,12 @@
-use std::net::SocketAddr;
+use std::{io, net::SocketAddr};
+
+use futures::future::join_all;
+use tokio::net::UdpSocket;
 
 pub struct DNSResolver {
     upstream: Vec<SocketAddr>,
     strategy: ResolutionStrategy,
+    counter: u16,
 }
 
 pub enum ResolutionStrategy {
@@ -15,6 +19,7 @@ impl Default for DNSResolver {
         Self {
             upstream: Default::default(),
             strategy: ResolutionStrategy::RoundRobin,
+            counter: Default::default(),
         }
     }
 }
@@ -28,6 +33,17 @@ impl DNSResolver {
             }
         }
     }
+
+    pub async fn send_request(&mut self, req: &[u8], socket: UdpSocket) -> Result<(), io::Error> {
+        self.counter = self.counter.wrapping_add(1);
+        let futures: Vec<_> = self
+            .pick_upstream(self.counter)
+            .iter()
+            .map(|upstream| socket.send_to(req, upstream))
+            .collect();
+        join_all(futures).await;
+        Ok(())
+    }
 }
 
 #[cfg(test)]
@@ -39,9 +55,23 @@ mod test {
         let resolver = DNSResolver {
             upstream: upstream.clone(),
             strategy: ResolutionStrategy::RoundRobin,
+            counter: 0,
         };
         assert_eq!(&[upstream[0]], resolver.pick_upstream(0));
         assert_eq!(&[upstream[1]], resolver.pick_upstream(1));
         assert_eq!(&[upstream[0]], resolver.pick_upstream(2));
+    }
+
+    #[test]
+    fn test_all() {
+        let upstream = vec!["1.1.1.1:53".parse().unwrap(), "1.0.0.1:53".parse().unwrap()];
+        let resolver = DNSResolver {
+            upstream: upstream.clone(),
+            strategy: ResolutionStrategy::All,
+            counter: 0,
+        };
+        assert_eq!(&upstream[..], resolver.pick_upstream(0));
+        assert_eq!(&upstream[..], resolver.pick_upstream(1));
+        assert_eq!(&upstream[..], resolver.pick_upstream(2));
     }
 }
